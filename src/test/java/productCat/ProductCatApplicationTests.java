be56ed7ee59d0be70b6102ed5c291c9f10289804
sumanthkumar44 <sumanthkumar44@gmail.com>
2021-01-04 13:34:31 +0530

    hi

---
+++ src/test/java/productCat/ProductCatApplicationTests.java
@@ -0,0 +1,13 @@
+package productCat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
